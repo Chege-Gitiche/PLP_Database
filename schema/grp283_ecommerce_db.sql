e0150f5d917e1e6d31746f91e922e4bcc9235edc
Mwika Roy <mwika.roy@technoincor.com>
2025-04-25 22:00:13 +0300

    Initial Commit for the grp283_ecommerce_db.sql

---
+++ schema/grp283_ecommerce_db.sql
@@ -0,0 +1,5 @@
+-- CREATE DATABASE grp283_ecommerce_db
+DROP DATABASE IF EXISTS grp283_ecommerce_db;
+CREATE DATABASE grp283_ecommerce_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE grp283_ecommerce_db;
+
